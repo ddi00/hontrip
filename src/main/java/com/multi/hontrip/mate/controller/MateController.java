0967de1addc99f731507d9a78455dd7bde8f1922
yanghyewon <ehfus963@gmail.com>
2023-08-08 14:56:42 +0900

    동행인 디벨롭 커밋

--- src/main/java/com/multi/hontrip/mate/controller/MateController.java
+++ src/main/java/com/multi/hontrip/mate/controller/MateController.java
@@ -1,7 +1,10 @@
 package com.multi.hontrip.mate.controller;
 
+import com.multi.hontrip.mate.service.MateService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MateController {
+
+    MateService mateService;
 }
