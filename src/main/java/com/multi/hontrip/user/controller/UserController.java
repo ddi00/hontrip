337ff6e235017ddf292d7d5ad962160ee6f70121
shj0212 <215212g@gmail.com>
2023-08-08 13:27:48 +0900

    [fix]git push 테스트1

--- src/main/java/com/multi/hontrip/user/controller/UserController.java
+++ src/main/java/com/multi/hontrip/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.multi.hontrip.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
+    public void home(){
+        System.out.println("userHome");
+    }
 }
