7f99054d153b16f25abd3353704515b55fc88f13
wlsrn99 <wlsrn331@naver.com>
2023-08-08 15:17:06 +0900

    [test] merge test입니다..죄송합니다..

--- src/main/java/com/multi/hontrip/mate/dto/MateDTO.java
+++ src/main/java/com/multi/hontrip/mate/dto/MateDTO.java
@@ -1,5 +1,8 @@
 package com.multi.hontrip.mate.dto;
 
+import lombok.Data;
+
+@Data
 public class MateDTO {
 	
 	private long mateBoardId;
@@ -10,56 +13,5 @@ public class MateDTO {
 	private String finishDate;
 	private boolean isFinish;
 	private int recruitNumber;
-	
-	public long getMateBoardId() {
-		return mateBoardId;
-	}
-	public void setMateBoardId(long mateBoardId) {
-		this.mateBoardId = mateBoardId;
-	}
-	public int getRecruitNumber() {
-		return recruitNumber;
-	}
-	public void setRecruitNumber(int recruitNumber) {
-		this.recruitNumber = recruitNumber;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getThumbnale() {
-		return thumbnale;
-	}
-	public void setThumbnale(String thumbnale) {
-		this.thumbnale = thumbnale;
-	}
-	public String getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-	public String getFinishDate() {
-		return finishDate;
-	}
-	public void setFinishDate(String finishDate) {
-		this.finishDate = finishDate;
-	}
-	public boolean isFinish() {
-		return isFinish;
-	}
-	public void setFinish(boolean isFinish) {
-		this.isFinish = isFinish;
-	}
-	
-
 	
 }
