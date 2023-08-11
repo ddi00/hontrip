ce1aa69cd4d28adb035459b7ad8578cb045c3ecb
wlsrn99 <wlsrn331@naver.com>
2023-08-12 00:20:46 +0900

    [add] 매칭 게시판 목록에 페이징 기능, 검색 기능 추가

--- src/main/java/com/multi/hontrip/mate/controller/MateController.java
+++ src/main/java/com/multi/hontrip/mate/controller/MateController.java
@@ -1,16 +1,52 @@
 package com.multi.hontrip.mate.controller;
 
+import com.multi.hontrip.mate.dao.MateDAO;
+import com.multi.hontrip.mate.dto.LocationDTO;
+import com.multi.hontrip.mate.dto.MateBoardListDTO;
+import com.multi.hontrip.mate.dto.PageDTO;
 import com.multi.hontrip.mate.service.MateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MateController {
 
-    MateService mateService;
-    //good
+    @Autowired
+  private MateService mateService;
 
+    @Autowired
+    MateDAO mateDAO;
 
-    public void insert(){
-        System.out.println("강진구");
+    @RequestMapping("bbs_list")
+    public void list(PageDTO pageDTO, Model model) {
+        //start, end지점 구하기
+        pageDTO.setStartEnd(pageDTO.getPage());
+        //게시물 리스트 가져오기
+        List<MateBoardListDTO> list = mateService.list(pageDTO);
+        //게시물 개수 가져오기
+        int count = mateService.count();
+        System.out.println("all count >> " + count);
+        //페이지 수 게산
+        //1page당 5개의 게시물을 넣는 경우
+        int pages = mateService.pages(count);
+        model.addAttribute("list", list);
+        model.addAttribute("count", count);
+        model.addAttribute("pages", pages);
+
+        //지역 리스트 가져오기
+        List<LocationDTO> location = mateService.location();
+        model.addAttribute("location", location);
+
     }
-}
+
+//    @RequestMapping("bbs_one")
+//    public void one(int id, Model model) {
+//        BbsDTO dto = bbsDAO.one(id);
+//        List<ReplyDTO> list = replyDAO.list(id);
+//        model.addAttribute("dto", dto);
+//        model.addAttribute("list", list);
+    }
--- src/main/java/com/multi/hontrip/mate/service/MateServiceImpl.java
+++ src/main/java/com/multi/hontrip/mate/service/MateServiceImpl.java
@@ -1,7 +1,42 @@
 package com.multi.hontrip.mate.service;
 
+import com.multi.hontrip.mate.dao.MateDAO;
+import com.multi.hontrip.mate.dto.LocationDTO;
+import com.multi.hontrip.mate.dto.MateBoardListDTO;
+import com.multi.hontrip.mate.dto.PageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MateServiceImpl implements MateService{
-}
+    @Autowired
+    private MateDAO mateDAO;
+
+    @Override
+    public List<MateBoardListDTO> list(PageDTO pageDTO) {
+        pageDTO.setStartEnd(pageDTO.getPage());
+        return mateDAO.list(pageDTO);
+    }
+    @Override
+    public int pages(int count) {
+        int pages = 0;
+        if(count % 10 == 0) {
+            pages = count / 5; //120개 --> 12pages
+        }else {
+            pages = count / 5 + 1; //122개 --> 13pages
+        }
+        return pages;
+    }
+    @Override
+    public int count() {
+        return mateDAO.count();
+    }
+
+    @Override
+    public List<LocationDTO> location() {
+        return mateDAO.location();
+    }
+    }
+
