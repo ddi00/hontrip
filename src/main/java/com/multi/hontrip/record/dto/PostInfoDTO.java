c319a944de0d272b52723cfd06dd47971e0d3084
고영준 <>
2023-08-12 15:26:57 +0900

    [record][add] 게시물 상세페이지 dto추가

---
+++ src/main/java/com/multi/hontrip/record/dto/PostInfoDTO.java
@@ -0,0 +1,25 @@
+package com.multi.hontrip.record.dto;
+
+import lombok.Data;
+
+
+@Data
+public class PostInfoDTO {
+    private long boardId;
+    private String city;
+    private String nickName;
+    private String title;
+    private String imgUrl;
+    private String content;
+    private String thumbnail;
+    private String createdAt;
+    private String updatedAt;
+    private int likeCount;
+    private String cmtUserNickName;
+    private String cmtContent;
+    private int cmtSequence;
+    private int cmtsSquence;
+    private int indentationNum;
+    private String cmtCreatedAt;
+    private String cmtUpdatedAt;
+}
