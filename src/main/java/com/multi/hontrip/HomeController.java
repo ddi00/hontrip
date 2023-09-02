714c7ef4e9c1ff50d22d100e97b9aec81acf88d9
고영준 <>
2023-09-02 09:29:58 +0900

    [record][add] 홈 컨트롤러 추가

--- src/main/java/com/multi/hontrip/HomeController.java
+++ src/main/java/com/multi/hontrip/HomeController.java
@@ -2,12 +2,17 @@ package com.multi.hontrip;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import com.multi.hontrip.record.dto.PostInfoDTO;
+import com.multi.hontrip.record.service.RecordService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,22 +20,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	private final RecordService recordService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		List<PostInfoDTO> topList = recordService.likeTopTen();
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+
+
+		model.addAttribute("topList", topList);
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
